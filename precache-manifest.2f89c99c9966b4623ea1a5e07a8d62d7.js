e4710d5624133bfaae7a03bbf5aec51909851471
robmcelhinney <robmcelhinney@hotmail.com>
2019-05-19 21:38:42 +0100

    Updates

---
+++ precache-manifest.2f89c99c9966b4623ea1a5e07a8d62d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52938f60b70a5da99256302832f81641",
+    "url": "/MiddleEarthSearch/index.html"
+  },
+  {
+    "revision": "46c441a754fc2dfc840d",
+    "url": "/MiddleEarthSearch/static/css/main.72a986a9.chunk.css"
+  },
+  {
+    "revision": "07c403e5f1c38898f4f8",
+    "url": "/MiddleEarthSearch/static/js/2.0b77da72.chunk.js"
+  },
+  {
+    "revision": "46c441a754fc2dfc840d",
+    "url": "/MiddleEarthSearch/static/js/main.24290f22.chunk.js"
+  },
+  {
+    "revision": "d847b0605ab4f829ae36",
+    "url": "/MiddleEarthSearch/static/js/runtime~main.92b223e3.js"
+  },
+  {
+    "revision": "86181de9f143dca1fae93bd617bdce65",
+    "url": "/MiddleEarthSearch/static/media/balrog.86181de9.jpg"
+  }
+]);
